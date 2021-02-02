5d20e65fde594788d1a9eb23a14aa76349889562
Sergej <sergejgritsenya@gmail.com>
2021-02-02 23:30:04 +0300

    fix middleware

--- src/middlewares/collector.middleware.ts
+++ src/middlewares/collector.middleware.ts
@@ -4,8 +4,9 @@ import { NextFunction, Request, Response } from "express"
 export const collectorMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
   try {
     const url = process.env.COLLECTOR_URL
+    console.log(req.ip)
     if (url) {
-      await axios.post(url, { data: { id: req.ip } })
+      await axios.post(url, { ip: req.ip })
     }
     next()
   } catch (_err) {
